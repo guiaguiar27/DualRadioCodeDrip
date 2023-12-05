52e70db060cceabff8df316d92ac16fb7b42080f
guiaguiar27 <guigasilva27@gmail.com>
2023-12-04 22:42:17 -0300

    all changes on the place

--- Logs/test.py
+++ Logs/test.py
@@ -1,9 +1,24 @@
-# import re
-# import scipy.stats
-# import numpy as np
-# import sys 
+import re
+import scipy.stats
+import numpy as np
+import sys   
+import statistics
 
-# to calculate confidence interval  
+# to calculate confidence interval   
+def conf(data): 
+        # Calculate sample mean and standard deviation
+    mean = np.mean(data)
+    std_dev = np.std(data, ddof=1)  # ddof=1 for sample standard deviation
+
+    # Set the confidence level and calculate the confidence interval
+    confidence_level = 0.95
+    alpha = 1 - confidence_level
+    z_score = scipy.stats.norm.ppf(1 - alpha / 2)  # Z-score for a normal distribution
+
+    # Calculate the margin of error
+    margin_of_error = z_score * (std_dev / np.sqrt(len(data)))
+ 
+    return margin_of_error
 def confident_interval_data(X, confidence = 0.95, sigma = -1):
     def S(X): #funcao para calcular o desvio padrao amostral
         s = 0
@@ -55,7 +70,10 @@ def extract_value(words):
             if w == "rx_count:": 
                 return words[i+1]
 
-
+def extract_value_tx(words): 
+   for i,w in enumerate(words): 
+            if w == "tx:":  
+                return words[i+1]
 def get_number(source):  
     #print(source)
     first = str(source[1]) 
@@ -67,7 +85,8 @@ def extract_node(source):
     for i,w in enumerate(words): 
             if w == "rx_count:": 
                 #print(words[1]) 
-                return words[i-1] 
+                return words[i-1]  
+    
 
 # adciona todos os pacotes em uma lista para ficar mais facil de ser processado 
 def add_list_nodes(listRef, value, index): 
@@ -84,13 +103,31 @@ def sum_list(listRef):
     sum = 0 
     for i in listRef: 
         sum += i 
-    return sum
+    return sum 
+
+def find_line_with_string(file_path, target_string):
+    sum = 0 
+    with open(file_path, 'r') as file: 
+
+        for line_number, line in enumerate(file, 1):
+            if target_string in line: 
+                sum +=1  
+        return sum
+
+
+
 
 i = 1  
 max = 10
 packet_size = 125 
-thr_list = []
+thr_list = [] 
+pdr_list = []
 thr_list_size = max  
+flag_test = 1
+if flag_test == 0: 
+    flag  = "o" 
+else: 
+    flag =  ""
 sizes = [10,20,30,40,50]
 for size in  sizes: 
     for i in range(1,max+1): 
@@ -101,15 +138,20 @@ for size in  sizes:
         # get the packet 
 
         file_name = f"{size}Nlog{i}"  
-        path_to_file =   f"/Users/guilhermeaguiar/Desktop/DualRadioCodeDrip/Logs/{size}/" + file_name + ".txt"
-        #print(path_to_file) 
-        
+        path_to_file =   f"/Users/guilhermeaguiar/Desktop/DualRadioCodeDrip/Logs/{size}{flag}/" + file_name + ".txt" 
+        #print(path_to_file)
+
+        num_packet_sent = 0 
+
         with open(path_to_file) as tf: 
             for line in tf:   
-                
-                num_packet_recpt = 0
-                words = line.split()  
-
+            
+                num_packet_recpt = 0    
+                words = line.split() 
+               
+                if "tx:" in words:  
+                    num_packet_sent += int(extract_value_tx(words))*2 
+                    #print(num_packet_sent)
                 if "rx_count:" in words:   
                     #extract_node(words)  
                     node = get_number(words) 
@@ -131,12 +173,27 @@ for size in  sizes:
                     #print(my_list[:])  
                     #print(sum_list(my_list)) 
 
-        #print(final_time -start_time)
-        thr = (sum_list(my_list) * packet_size)/((final_time - start_time)/1000) 
+        
+        if(flag_test == 0):
+            recpt_to_pdr  = find_line_with_string(path_to_file, " rx ") 
+        else: 
+            recpt_to_pdr = sum(my_list)    
+         
+        pdr = (recpt_to_pdr/num_packet_sent) * 100  
+        if pdr>100:  pdr = 100
+
+        pdr_list.append(pdr)  
+   
+        thr = (recpt_to_pdr * packet_size)/((final_time - start_time)/1000) 
         #print(thr)
         thr_list.append(thr)          
-
+ 
     #print(thr_list[:]) 
-    print(average(thr_list))
-            #print(f"Total num packets:{num_packet_recpt}")
+    #print(confident_interval_data(thr_list,0.95)) 
+    #print(thr_list[:])
+    #print(average(thr_list))
+    #print(average(pdr_list)) 
+    #print(conf(thr_list)) 
+    print(conf(pdr_list))
+
 

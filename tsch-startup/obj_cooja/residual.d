7cb5d00d0110f564674717fd98b092b269ade208
guiaguiar27 <guigasilva27@gmail.com>
2024-04-01 10:04:02 -0300

    updates

--- tsch-startup/obj_cooja/residual.d
+++
@@ -1,6 +0,0 @@
-obj_cooja/residual.o: ../../os/net/mac/residual.c \
- ../../os/net/mac/residual.h ../../arch/platform/cooja/./contiki-conf.h \
- project-conf.h
-../../os/net/mac/residual.c :
- ../../os/net/mac/residual.h ../../arch/platform/cooja/./contiki-conf.h :
- project-conf.h :

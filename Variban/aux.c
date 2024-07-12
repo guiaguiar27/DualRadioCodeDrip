a79b59354057fdc9854ba37385b152a8755bbbf2
guiaguiar27 <guigasilva27@gmail.com>
2024-07-12 00:10:59 -0300

    comit

--- Variban/aux.c
+++ Variban/aux.c
@@ -25,3 +25,28 @@ void tsch_write_in_file(int n_origin, int n_destin){
   fprintf(file, "%d %d\n",n_origin,n_destin);
   fclose(file);
 } 
+
+
+
+// funcao leitura 
+int find_y_for_x(const char *filename, int x) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    printf("Erro ao abrir o arquivo %s\n", filename);
+    return -1;
+  }
+
+  int x_read, y_read;
+  while (fscanf(file, "%d %d", &x_read, &y_read) != EOF) {
+    if (x_read == x) {
+      fclose(file);
+      return y_read;
+    }
+  }
+
+  fclose(file);
+  return -1; // Retorna -1 se não encontrar o valor
+}
+
+// - cria dois canais de broadcast 
+// - faz rapida descoberta de vizinhos via time source 

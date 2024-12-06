928157c025f2262a91e021580b4f72288e167220
guiaguiar27 <guigasilva27@gmail.com>
2024-12-06 15:08:59 -0300

    modules for other rules

--- Dualchestra/init.c
+++ Dualchestra/init.c
@@ -1,84 +1,141 @@
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#include "tsch_neighbor.h"
-#include "linkaddr.h"
-#include "net/mac/tsch/tsch.h"
-#include "net/mac/tsch/tsch-schedule.h"
+/*
+ * Copyright (c) 2016, Inria.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
-/* Function declarations */
-void init_function(uint16_t slotframe_handle) {
-  struct tsch_slotframe *sf = tsch_schedule_add_slotframe(slotframe_handle, 10); // Example period
-  if (sf == NULL) {
-    fprintf(stderr, "Error: Failed to add slotframe.\n");
-    return;
+/**
+ * \file
+ *         Orchestra: a slotframe dedicated to unicast data transmission.
+ *         Designed primarily for RPL non-storing mode but would work with any mode-of-operation.
+ *         Uses uIPv6 DS6 neighbor tables to manage child nodes.
+ *
+ * \author Simon Duquennoy <simon.duquennoy@inria.fr>
+ *         Atis Elsts <atis.elsts@edi.lv>
+ */
+
+#include "contiki.h"
+#include "orchestra.h"
+#include "net/ipv6/uip-ds6-route.h"
+#include "net/packetbuf.h"
+#include "stdlib.h"
+
+static uint16_t slotframe_handle = 0;
+static struct tsch_slotframe *sf_unicast = NULL;
+
+/*---------------------------------------------------------------------------*/
+/* Helper Functions */
+static uint16_t get_node_timeslot(const linkaddr_t *addr) {
+  if (addr != NULL && ORCHESTRA_UNICAST_PERIOD > 0) {
+    return ORCHESTRA_LINKADDR_HASH(addr) % ORCHESTRA_UNICAST_PERIOD;
   }
-  tsch_schedule_add_link(sf, LINK_OPTION_TX | LINK_OPTION_RX, LINK_TYPE_NORMAL, &tsch_broadcast_address, 1, 0);
+  return 0xffff;
 }
 
-void new_time_source_function(const struct tsch_neighbor *old, const struct tsch_neighbor *new) {
-  if (new != NULL) {
-    printf("New time source: ");
-    linkaddr_print(&new->addr);
-    printf("\n");
+static uint16_t get_node_channel_offset(const linkaddr_t *addr) {
+  if (addr != NULL && ORCHESTRA_UNICAST_MAX_CHANNEL_OFFSET >= ORCHESTRA_UNICAST_MIN_CHANNEL_OFFSET) {
+    return ORCHESTRA_LINKADDR_HASH(addr) %
+           (ORCHESTRA_UNICAST_MAX_CHANNEL_OFFSET - ORCHESTRA_UNICAST_MIN_CHANNEL_OFFSET + 1)
+           + ORCHESTRA_UNICAST_MIN_CHANNEL_OFFSET;
+  }
+  return 0xffff;
+}
+
+/*---------------------------------------------------------------------------*/
+/* Rule Functions */
+static void init_function(uint16_t handle) {
+  slotframe_handle = handle;
+  sf_unicast = tsch_schedule_add_slotframe(slotframe_handle, ORCHESTRA_UNICAST_PERIOD);
+  if (sf_unicast != NULL) {
+    uint16_t rx_timeslot = get_node_timeslot(&linkaddr_node_addr);
+    tsch_schedule_add_link(sf_unicast, LINK_OPTION_RX, LINK_TYPE_NORMAL,
+                           &tsch_broadcast_address, rx_timeslot,
+                           get_node_channel_offset(&linkaddr_node_addr), 1);
+  }
+}
+
+static void new_time_source_function(const struct tsch_neighbor *old, const struct tsch_neighbor *new) {
+  const linkaddr_t *old_addr = tsch_queue_get_nbr_address(old);
+  const linkaddr_t *new_addr = tsch_queue_get_nbr_address(new);
+  if (old_addr) {
+    tsch_schedule_remove_link_by_offsets(sf_unicast, get_node_timeslot(old_addr), 0);
+  }
+  if (new_addr) {
+    tsch_schedule_add_link(sf_unicast, LINK_OPTION_SHARED | LINK_OPTION_TX,
+                           LINK_TYPE_NORMAL, &tsch_broadcast_address,
+                           get_node_timeslot(new_addr), 0, 0);
+  }
+}
+
+static int select_packet_function(uint16_t *slotframe, uint16_t *timeslot, uint16_t *channel_offset) {
+  const linkaddr_t *dest = packetbuf_addr(PACKETBUF_ADDR_RECEIVER);
+  if (packetbuf_attr(PACKETBUF_ATTR_FRAME_TYPE) == FRAME802154_DATAFRAME
+      && !linkaddr_cmp(dest, &linkaddr_null)) {
+    if (slotframe) *slotframe = slotframe_handle;
+    if (timeslot) *timeslot = get_node_timeslot(dest);
+    if (channel_offset) *channel_offset = get_node_channel_offset(dest);
+    return 1;
+  }
+  return 0;
+}
+
+static void neighbor_updated_function(const linkaddr_t *addr, uint8_t is_added) {
+  if (is_added) {
+    tsch_schedule_add_link(sf_unicast, LINK_OPTION_SHARED | LINK_OPTION_TX,
+                           LINK_TYPE_NORMAL, &tsch_broadcast_address,
+                           get_node_timeslot(addr), 0, 0);
   } else {
-    printf("Time source removed.\n");
+    tsch_schedule_remove_link_by_offsets(sf_unicast, get_node_timeslot(addr), 0);
   }
 }
 
-// pode ser alterado
-int select_packet_function(uint16_t *slotframe, uint16_t *timeslot) {
-  *slotframe = 0; 
-  *timeslot = 1;  
-  return 1;      
-}
-
-void child_added_function(const linkaddr_t *addr) {
-  printf("Child added: ");
-  linkaddr_print(addr);
-  printf("\n");
-}
-
-void child_removed_function(const linkaddr_t *addr) {
-  printf("Child removed: ");
-  linkaddr_print(addr);
-  printf("\n");
-}
-
-/* Structure initialization */
+/*---------------------------------------------------------------------------*/
+/* Rule Initialization */
 struct orchestra_rule* create_orchestra_rule() {
-  struct orchestra_rule* rule = malloc(sizeof(struct orchestra_rule));
-  if (!rule) {
-    fprintf(stderr, "Error: Failed to allocate memory for orchestra_rule.\n");
-    return NULL;
+  struct orchestra_rule *rule = malloc(sizeof(struct orchestra_rule));
+  if (rule) {
+    rule->init = init_function;
+    rule->new_time_source = new_time_source_function;
+    rule->select_packet = select_packet_function;
+    rule->neighbor_updated = neighbor_updated_function;
+    rule->name = "unicast per neighbor non-storing";
+    rule->slotframe_length = ORCHESTRA_UNICAST_PERIOD;
   }
-  rule->init = init_function;
-  rule->new_time_source = new_time_source_function;
-  rule->select_packet = select_packet_function;
-  rule->child_added = child_added_function;
-  rule->child_removed = child_removed_function;
   return rule;
 }
 
-extern struct orchestra_rule *orchestra_rules[];
-void register_orchestra_rule(struct orchestra_rule *rule) {
-  static int rule_count = 0;
-  if (rule_count < ORCHESTRA_RULES_COUNT) {
-    orchestra_rules[rule_count++] = rule;
-  } else {
-    fprintf(stderr, "Error: Cannot register more orchestra rules.\n");
-  }
-}
-
-int main() {
+void orchestra_init() {
   struct orchestra_rule* rule = create_orchestra_rule();
   if (rule == NULL) {
-    return -1;
+    return ;
   }
 
-  register_orchestra_rule(rule);
-  rule->init(0);  
+  //register_orchestra_rule(rule);
+  rule->init(0);
 
   free(rule);
-  return 0;
+
 }
--- Dualchestra/orchestra.c
+++ Dualchestra/orchestra.c
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright (c) 2015, Swedish Institute of Computer Science.
  * All rights reserved.
  *
@@ -46,14 +46,11 @@
 #elif ROUTING_CONF_RPL_CLASSIC
 #include "net/routing/rpl-classic/rpl.h"
 #endif
+#include "stdlib.h"
 
-#define DEBUG DEBUG_PRINT
+
 #include "net/ipv6/uip-debug.h"
 
-/* A net-layer sniffer for packets sent and received */
-static void orchestra_packet_received(void);
-static void orchestra_packet_sent(int mac_status);
-NETSTACK_SNIFFER(orchestra_sniffer, orchestra_packet_received, orchestra_packet_sent);
 
 /* The current RPL preferred parent's link-layer address */
 linkaddr_t orchestra_parent_linkaddr;
@@ -61,110 +58,82 @@ linkaddr_t orchestra_parent_linkaddr;
 int orchestra_parent_knows_us = 0;
 
 /* The set of Orchestra rules in use */
-const struct orchestra_rule *all_rules[] = ORCHESTRA_RULES;
-#define NUM_RULES (sizeof(all_rules) / sizeof(struct orchestra_rule *))
+// const struct orchestra_rule *all_rules[] = ORCHESTRA_RULES;
+// #define NUM_RULES (sizeof(all_rules) / sizeof(struct orchestra_rule *))
 
-/*---------------------------------------------------------------------------*/
-static void
-orchestra_packet_received(void)
-{
-}
-/*---------------------------------------------------------------------------*/
-static void
-orchestra_packet_sent(int mac_status)
-{
-  /* Check if our parent just ACKed a DAO */
-  if(orchestra_parent_knows_us == 0
-     && mac_status == MAC_TX_OK
-     && packetbuf_attr(PACKETBUF_ATTR_NETWORK_ID) == UIP_PROTO_ICMP6
-     && packetbuf_attr(PACKETBUF_ATTR_CHANNEL) == (ICMP6_RPL << 8 | RPL_CODE_DAO)) {
-    if(!linkaddr_cmp(&orchestra_parent_linkaddr, &linkaddr_null)
-       && linkaddr_cmp(&orchestra_parent_linkaddr, packetbuf_addr(PACKETBUF_ADDR_RECEIVER))) {
-      orchestra_parent_knows_us = 1;
-    }
+void init_function(uint16_t slotframe_handle) {
+  struct tsch_slotframe *sf = tsch_schedule_add_slotframe(slotframe_handle, 10); // Example period
+  if (sf == NULL) {
+    fprintf(stderr, "Error: Failed to add slotframe.\n");
+    return;
   }
+  tsch_schedule_add_link(sf, LINK_OPTION_TX | LINK_OPTION_RX, LINK_TYPE_NORMAL, &tsch_broadcast_address, 1, 0);
 }
-/*---------------------------------------------------------------------------*/
-void
-orchestra_callback_child_added(const linkaddr_t *addr)
-{
-  /* Notify all Orchestra rules that a child was added */
-  int i;
-  for(i = 0; i < NUM_RULES; i++) {
-    if(all_rules[i]->child_added != NULL) {
-      all_rules[i]->child_added(addr);
-    }
-  }
-}
-/*---------------------------------------------------------------------------*/
-void
-orchestra_callback_child_removed(const linkaddr_t *addr)
-{
-  /* Notify all Orchestra rules that a child was removed */
-  int i;
-  for(i = 0; i < NUM_RULES; i++) {
-    if(all_rules[i]->child_removed != NULL) {
-      all_rules[i]->child_removed(addr);
-    }
-  }
-}
-/*---------------------------------------------------------------------------*/
-void
-orchestra_callback_packet_ready(void)
-{
-  int i;
-  /* By default, use any slotframe, any timeslot */
-  uint16_t slotframe = 9;
-  uint16_t timeslot = 0xffff;
 
-  /* Loop over all rules until finding one able to handle the packet */
-  for(i = 0; i < NUM_RULES; i++) {
-    if(all_rules[i]->select_packet != NULL) {
-      if(all_rules[i]->select_packet(&slotframe, &timeslot)) {
-        break;
-      }
-    }
-  }
-
-#if TSCH_WITH_LINK_SELECTOR
-  packetbuf_set_attr(PACKETBUF_ATTR_TSCH_SLOTFRAME, slotframe);
-  packetbuf_set_attr(PACKETBUF_ATTR_TSCH_TIMESLOT, timeslot);
-#endif
-}
-/*---------------------------------------------------------------------------*/
-void
-orchestra_callback_new_time_source(const struct tsch_neighbor *old, const struct tsch_neighbor *new)
-{
-  /* Orchestra assumes that the time source is also the RPL parent.
-   * This is the case if the following is set:
-   * #define RPL_CALLBACK_PARENT_SWITCH tsch_rpl_callback_parent_switch
-   * */
-
-  int i;
-  if(new != old) {
-    orchestra_parent_knows_us = 0;
-  }
-  for(i = 0; i < NUM_RULES; i++) {
-    if(all_rules[i]->new_time_source != NULL) {
-      all_rules[i]->new_time_source(old, new);
-    }
+void new_time_source_function(const struct tsch_neighbor *old, const struct tsch_neighbor *new) {
+  if (new != NULL) {
+    printf("New time source: ");
+    //linkaddr_print(&new->addr);
+    printf("\n");
+  } else {
+    printf("Time source removed.\n");
   }
 }
-/*---------------------------------------------------------------------------*/
-void
-orchestra_init(void)
-{
-  int i;
-  /* Snoop on packet transmission to know if our parent knows about us
-   * (i.e. has ACKed at one of our DAOs since we decided to use it as a parent) */
-  netstack_sniffer_add(&orchestra_sniffer);
-  linkaddr_copy(&orchestra_parent_linkaddr, &linkaddr_null);
-  /* Initialize all Orchestra rules */
-  for(i = 0; i < NUM_RULES; i++) {
-    if(all_rules[i]->init != NULL) {
-      PRINTF("Orchestra: initializing rule %u\n", i);
-      all_rules[i]->init(i);
-    }
+
+// pode ser alterado
+int select_packet_function(uint16_t *slotframe, uint16_t *timeslot) {
+  *slotframe = 0;
+  *timeslot = 1;
+  return 1;
+}
+
+void child_added_function(const linkaddr_t *addr) {
+  printf("Child added: ");
+  //linkaddr_print(addr);
+  printf("\n");
+}
+
+void child_removed_function(const linkaddr_t *addr) {
+  printf("Child removed: ");
+  //linkaddr_print(addr);
+  printf("\n");
+}
+
+/* Structure initialization */
+struct orchestra_rule* create_orchestra_rule() {
+  struct orchestra_rule* rule = malloc(sizeof(struct orchestra_rule));
+  if (!rule) {
+    fprintf(stderr, "Error: Failed to allocate memory for orchestra_rule.\n");
+    return NULL;
   }
-  PRINTF("Orchestra: initialization done\n");
+  rule->init = init_function;
+  rule->new_time_source = new_time_source_function;
+  rule->select_packet = select_packet_function;
+  rule->child_added = child_added_function;
+  rule->child_removed = child_removed_function;
+  printf("Sucess in the allocation\n");
+  return rule;
+}
+
+// extern struct orchestra_rule *orchestra_rules[];
+// void register_orchestra_rule(struct orchestra_rule *rule) {
+//   static int rule_count = 0;
+//   if (rule_count < 3) {
+//     orchestra_rules[rule_count++] = rule;
+//   } else {
+//     fprintf(stderr, "Error: Cannot register more orchestra rules.\n");
+//   }
+// }
+
+void orchestra_init() {
+  struct orchestra_rule* rule = create_orchestra_rule();
+  if (rule == NULL) {
+    return ;
+  }
+
+  //register_orchestra_rule(rule);
+  rule->init(0);
+
+  free(rule);
+
 }
